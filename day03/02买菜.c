512785cbd80cc3b019f51bd16214aea868c147cf
wh-991213 <71365519+wh-991213@users.noreply.github.com>
2020-11-07 18:12:45 +0800

    Update 02买菜.c
    Add方法3

--- day03/02买菜.c
+++ day03/02买菜.c
@@ -1,23 +1,25 @@
-//��VS��ʹ��scanf��strcpy��sprintf�����ȣ�����ʾC4996����
-//����1������ڿ�ͷ��
+//在VS中使用scanf、strcpy、sprintf函数等，会提示C4996错误
+//方法1（需放在开头）
 #define _CRT_SECURE_NO_WARNINGS
-//����2
+//方法2
 #pragma warning(disable:4996)
+//方法3
+//在使用scanf函数时，使用scanf_s替代，但不采取。因为scanf_s并不是C语言中的函数，而是VS编译器下的。也就是说在VS中使用scanf_s可行，但是在其它编译器下不一定可行
 #include<stdio.h>
 
 int main0201(viod)
 
 {
-	//�ƹ�3Ԫ/�� 
-	//�������ڳ������й����У���ֵ���ܷ����ı����
-	//const��Լ��ʹ��Ϊ����
+	//黄瓜3元/斤 
+	//常量：在程序运行过程中，其值不能发生改变的量
+	//const：约束使其为常量
 	const int price = 3;
 	//price = 5;//err
 
-	//�������ڳ������й����У���ֵ���Է����ı����
+	//变量：在程序运行过程中，其值可以发生改变的量
 	int weight;
-	printf("�����빺�������\n");
-	//scanf��ͨ���������븳ֵ��&��ȡ��ַ�����
+	printf("请输入购买斤数：\n");
+	//scanf：通过键盘输入赋值；&：取地址运算符
 	scanf("%d", &weight);
 
 	int sum = price * weight;
